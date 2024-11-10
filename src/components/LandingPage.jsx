851b33fced026254b068d2588ef970841d43f009
YeatisNeat <awaisiscool@yahoo.com>
2024-11-10 21:09:34 +0500

    Worked on the landing page some more. Added spaces for illustration and forms.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,20 +2,30 @@ import './LandingPage.css'
 import LOGO from '../assets/logo.svg'
 import GLOBE from '../assets/globe.png'
 import QUESTOIN from '../assets/question.png'
+import ILLUSTRATION from '../assets/illustration.png'
 
 const LandingPage = () => {
     return (
-        <div className='header'>
-            <div className='left-elements'>
-                <img className='logo' src={LOGO} alt='LOGO'></img>
-                <p>LOGO</p>
+        <>
+            <div className='header'>
+                <div className='left-elements'>
+                    <img className='logo' src={LOGO} alt='LOGO'></img>
+                    <p>LOGO</p>
+                </div>
+                <div className='right-elements'>
+                    <img className='globe' src={GLOBE} alt='GLOBE'></img>
+                    <img className='question' src={QUESTOIN} alt='question'></img>
+                </div>
             </div>
-            <div className='right-elements'>
-                <img className='globe' src={GLOBE} alt='GLOBE'></img>
-                <img className='question' src={QUESTOIN} alt='question'></img>
+
+            <div className="main-content">
+                <div className="grid-container">
+                    <img className='illustration' src={ILLUSTRATION}></img>
+                </div>
             </div>
-            
-        </div>
+        </>
+
+        
     )
 }
 
