849d65afd2696d603e283f90e66ad5ee6dcf1007
YeatisNeat <awaisiscool@yahoo.com>
2024-11-13 14:51:33 +0500

    Added Input fields
    Added and styled the input fields so far.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,36 @@
+import './Login.css'
+import WAVE from '../assets/wave.png'
+import MAIL from '../assets/mail.png'
+import LOCK from '../assets/lock.png'
+import EYE from '../assets/eye.svg'
+
 const Login = () => {
     return (
         <>
-            <p>Hello, World</p>
+            <form>
+                <div>
+                    <h1>Welcome back</h1>
+                    {/* Add the wave icon here */}
+                    {/* <img src={WAVE} alt='Wave.png'></img> */}
+                </div>
+
+                <h2>Log in your account</h2>
+
+                <div className="input-email">
+                    <img src={MAIL} alt='mail.png'></img>
+                    <input type="text" placeholder='jrobinson@hotmail.com'
+                    pattern='^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'>
+                    </input>
+                </div>
+
+                <div className="input-password">
+                    <img src={LOCK} alt='lock.png'></img>
+                    <input type="password" placeholder='Hellojrobinson@123'>
+                    </input>
+                    <img src={EYE} alt='eye.png'></img>
+                </div>
+                
+            </form>
         </>
     )
 }
